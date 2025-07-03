addf0f99ed6bde7c965907d1b52e4f5e6c739159
Kiran Kumar <kirankumar@dhcp-10-120-146-208.wifi.oracle.com>
2025-07-04 00:03:34 +0530

    Day - 8

---
+++ 2nd July 2025/script.js
@@ -0,0 +1,53 @@
+
+// console.log(a);
+
+
+var a; // declare
+a = 20;  // initialize 
+
+
+
+// Till ES5 
+var fname; 
+fname = "Jyoti"
+var fname = "Parth" // global, redeclare, reinitialize 
+var fname = "Arjun"
+fname = "Muktesh"
+console.log(fname);
+
+
+// ES6 - most lovable & error free. 
+// let, const 
+
+
+
+// const pi = 3.144444  // declare & initialize dono ek time pe karna hota hai
+                     // redeclare ya reinitialize. 
+
+pi = 5.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = 20;      // a = 20 - global 
+
+let a = 30;  
+a = 30; 
+
+
+// const a = 30;  
+
+// console.log(a);
+
+
+
+
